7b3c7d4624bb05766f230826ecc4c1c6fc417dd5
Igorio29 <igorjosefarias3@gmail.com>
2023-09-27 17:48:43 -0300

    Pagina de Entrada completa

--- Tela-de-inicio/JavaScript.js
+++ Tela-de-inicio/JavaScript.js
@@ -7,3 +7,23 @@ function mostrarTexto(id) {
         regra.style.display = 'block';
     }
 }
+
+window.addEventListener("scroll", function() {
+    var footer = document.getElementById("footer");
+    if (window.scrollY > 100) {
+        footer.classList.remove("hidden");
+    } else {
+        footer.classList.add("hidden");
+    }
+});
+
+document.getElementById("back-to-top").addEventListener("click", function() {
+    var smoothScroll = function() {
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothScroll);
+            window.scrollTo(0, currentScroll - currentScroll / 8);
+        }
+    };
+    smoothScroll();
+});
